c8263524199fd8f41d62859c184181b0b31562bc
Shabadoo9 <brinstonzbpe@gmail.com>
2023-10-30 16:09:45 -0400

    added all files and completed shapes.js

---
+++ assets/shapes.js
@@ -0,0 +1,43 @@
+class Shape {
+    constructor() {
+      this.color = "";
+    }
+  
+    setColor(color) {
+      this.color = color;
+    }
+  }
+  
+  class TextColor {
+    constructor() {
+      this.color = "";
+    }
+  
+    setColor(color) {
+      this.color = color;
+    }
+  
+    render(text) {
+      return `<text x="150" y="115" fill="${this.color}" font-size="30" font-family="Arial">${text}</text>`;
+    }
+  }
+  
+  class Triangle extends Shape {
+    render() {
+      return `<polygon points="150, 18 244, 182 56, 182" fill="${this.color}" />`;
+    }
+  }
+  
+  class Square extends Shape {
+    render() {
+      return `<rect x="73" y="40" width="160" height="160" fill="${this.color}" />`;
+    }
+  }
+  
+  class Circle extends Shape {
+    render() {
+      return `<circle cx="150" cy="115" r="80" fill="${this.color}" />`;
+    }
+  }
+  
+  module.exports = { Triangle, Square, Circle, TextColor };
